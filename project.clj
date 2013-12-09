783d4057121c285d648fe127c0b435fe0d1e7691
William Roe <willroe@gmail.com>
2013-12-09 15:58:40 +0000

    Include url to the project page in metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hipchat "0.1.0-SNAPSHOT"
   :description "Hipchat client for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/owainlewis/hipchat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
