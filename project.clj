2f5985b9221cea1775dd505163e33fa3f6b1526d
Owain Lewis <owain@owainlewis.com>
2014-12-07 21:47:49 +0000

    updates project

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/owainlewis/hipchat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {:clojars {:sign-releases false :url "https://clojars.org/repo/"}}
+
+  :repositories {:clojars {:sign-releases false :url "https://clojars.org/repo"}}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.2.0"]
